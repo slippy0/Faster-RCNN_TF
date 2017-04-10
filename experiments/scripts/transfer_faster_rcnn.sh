ab6efde59d2bfef7a4b0a2947e968b3fc8a55b0b
Daniel Ron <dron@alum.mit.edu>
2017-04-09 22:01:04 -0400

    Added script for running transfer learning

---
+++ experiments/scripts/transfer_faster_rcnn.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Usage:
+# ./experiments/scripts/faster_rcnn_end2end.sh GPU NET DATASET [options args to {train,test}_net.py]
+# DATASET is either pascal_voc or coco.
+#
+# Example:
+# ./experiments/scripts/faster_rcnn_end2end.sh 0 VGG_CNN_M_1024 pascal_voc \
+#   --set EXP_DIR foobar RNG_SEED 42 TRAIN.SCALES "[400, 500, 600, 700]"
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+
+DEV=$1
+DEV_ID=$2
+NET=$3
+
+array=( $@ )
+len=${#array[@]}
+EXTRA_ARGS=${array[@]:4:$len}
+EXTRA_ARGS_SLUG=${EXTRA_ARGS// /_}
+
+LOG="experiments/logs/faster_rcnn_end2end_${NET}_${EXTRA_ARGS_SLUG}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+time python -m pdb ./tools/train_net.py --device ${DEV} --device_id ${DEV_ID} \
+  --weights data/pretrain_model/VGG_imagenet.npy \
+  --source_imdb voc_2007_trainval \
+  --source_data_path data/driving_50k \
+  --target_imdb voc_2007_trainval \
+  --target_data_path data/KITTI \
+  --iters 70000 \
+  --cfg experiments/cfgs/faster_rcnn_end2end.yml \
+  --network VGGnet_train \
+  ${EXTRA_ARGS}
