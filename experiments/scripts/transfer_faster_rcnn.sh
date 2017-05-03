72d51b78c362e3b49eac0ff767e582438b43eaed
Daniel Ron <dron@alum.mit.edu>
2017-05-03 16:41:07 -0400

    Modified transfer learning script to use weights pretrained on driving 50k

--- experiments/scripts/transfer_faster_rcnn.sh
+++ experiments/scripts/transfer_faster_rcnn.sh
@@ -7,9 +7,6 @@
 # ./experiments/scripts/faster_rcnn_end2end.sh 0 VGG_CNN_M_1024 pascal_voc \
 #   --set EXP_DIR foobar RNG_SEED 42 TRAIN.SCALES "[400, 500, 600, 700]"
 
-set -x
-set -e
-
 export PYTHONUNBUFFERED="True"
 
 DEV=${1-gpu}
@@ -20,17 +17,13 @@ len=${#array[@]}
 EXTRA_ARGS=${array[@]:4:$len}
 EXTRA_ARGS_SLUG=${EXTRA_ARGS// /_}
 
-LOG="experiments/logs/faster_rcnn_end2end_${EXTRA_ARGS_SLUG}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
-exec &> >(tee -a "$LOG")
-echo Logging output to "$LOG"
-
-time python -m pdb ./tools/train_transfer_net.py --device ${DEV} --device_id ${DEV_ID} \
-  --weights data/pretrain_model/VGG_imagenet.npy \
+time python ./tools/train_transfer_net.py --device ${DEV} --device_id ${DEV_ID} \
+  --weights data/pretrain_model/pretrained_200000.npy \
   --source_imdb transfer_source_trainval \
   --source_data_path data/driving_50k \
   --target_imdb transfer_target_trainval \
   --target_data_path data/KITTI \
-  --iters 70000 \
+  --iters 100000 \
   --cfg experiments/cfgs/faster_rcnn_end2end.yml \
   --network VGGnet_train \
   ${EXTRA_ARGS}
